fd8d079c219cb730b3592cad51b746a3e1028280
patrickgagnon <plgagnon00@gmail.com>
2020-05-06 15:16:21 -0400

    Adding gitignore and updating reqs

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+#requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="HIDUU",
+    version="0.0.1",
+    author="Patrick Gagnon",
+    author_email="plgagnon00@gmail.com",
+    description="A package to upload information to Cerner HealtheIntent",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/patrickgagnon/HIDUU",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8.2",
+        "License :: MIT",
+    ],
+)
