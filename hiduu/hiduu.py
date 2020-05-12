4653084d0e53dcd1ba89d637875b10f44beb61c4
patrickgagnon <plgagnon00@gmail.com>
2020-05-12 11:11:32 -0400

    Update #3

--- hiduu/hiduu.py
+++ hiduu/hiduu.py
@@ -1,10 +1,9 @@
 from subprocess import check_call
 import os
-from settings import *
 
 
-
-def hiduu_upload(upload_command = None,
+def hiduu_upload(hiduu_path = None,
+                 upload_command = None,
                  system_account_id = None,
                  system_account_secret = None,
                  source_id = None,
@@ -20,7 +19,7 @@ def hiduu_upload(upload_command = None,
                  args=None,
                  encryption = None,
                  client_mnemonic = None):
-        command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=HIDUU_PATH),
+        command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=hiduu_path),
                    "{upload_command}".format(upload_command=upload_command),
                    "-said",system_account_id,
                    "-sas",system_account_secret,
@@ -52,104 +51,35 @@ def hiduu_upload(upload_command = None,
         check_call(command)
 
 
-def upload_unvetted_file_unix(upload_sync = 'uploadUnvettedFile', 
-                              upload_sync_id = None,
-                              system_secret = None, 
-                              file_path = None,
-                              reason = None,
-                              release_date = None,
-                              sv = None,
-                              file_type_id = None, 
-                              data_source_id = None, 
-                              source_id = None):
-    command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=HIDUU_PATH),
-               "{upload_sync}".format(upload_sync=upload_sync),
-               "-said", source_id,
-               "-f", file_path,
-               "-sas", system_secret,
-               "-re", reason,
-               "-sid", upload_sync_id]
+def hiduu_upload_win(hiduu_path = None,
+                     upload_command = None,
+                     system_account_id = None,
+                     system_account_secret = None,
+                     source_id = None,
+                     data_set_id = None,
+                     file_id = None,
+                     file_release = None,
+                     spec_version = None,
+                     file = None,
+                     reason = None,
+                     sub_source = None,
+                     record_count = None,
+                     discovery = None,
+                     args = None,
+                     encryption = None,
+                     client_mnemonic = None):
+    command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=hiduu_path),
+               "{upload_command}".format(upload_command=upload_command),
+               " -said ", system_account_id,
+               " -sas ", system_account_secret,
+               " -sid ", source_id]
+    if upload_command == 'uploadUnvettedFile':
+        command +" -f " + file + " -re " + reason
 
-    if upload_sync == 'uploadUnvettedFile':
-        command.append("-rl") 
-        command.append(release_date)
-        command.append("-sv")
-        command.append(sv)
-        command.append("-fid")
-        command.append(file_type_id)
-        command.append("-dsid")
-        command.append(data_source_id)
+    if upload_command == 'uploadDataSetFile':
+        command + " -dsid " + data_set_id + " -sv " + spec_version + " -fid " + file_id + " -rl " + file_release + " -f " + file
 
-    check_call(command)
+    if upload_command == 'uploadReferenceFile':
+        command + " -rn " + data_set_id + " -f " + file + " -re " + reason
 
-
-
-
-
-def upload_dataset_file_unix(upload_sync = 'uploadDataSetFile', 
-                             upload_sync_id = None,
-                             system_secret = None,
-                             file_path = None, 
-                             release_date = None, 
-                             sv = None, 
-                             file_type_id = None,
-                             data_source_id = None, 
-                             source_id = None):
-    command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=HIDUU_PATH),
-               "{upload_sync}".format(upload_sync=upload_sync), "-said", source_id, "-sas", system_secret, "-sid",
-               upload_sync_id, "-f", file_path]
-
-    if upload_sync == 'uploadDataSetFile':
-        command.append("-rl")
-        command.append(release_date)
-        command.append("-sv")
-        command.append(sv)
-        command.append("-fid")
-        command.append(file_type_id)
-        command.append("-dsid")
-        command.append(data_source_id)
-        
-    check_call(command)
-
-
-def upload_reference_file_win(upload_sync = 'uploadReferenceFile', 
-                               source_id = None, 
-                               system_secret = None,
-                               upload_sync_id = None,
-                               file_name = None, 
-                               file_path = None, 
-                               reason = None):
-    command = "{hiduu_path}hi-data-upload-utility".format(
-      hiduu_path=HIDUU_PATH) + upload_sync + "-said" + source_id + "-sas" + system_secret + "-sid" + upload_sync_id + "-rn" + file_name + "-f" + file_path, "-re" + reason
     os.system(command)
-
-
-
-
-def upload_unvetted_file_win(upload_sync = 'uploadUnvettedFile', 
-                              upload_sync_id = None,
-                              system_secret = None, 
-                              file_path = None, reason = None, 
-                              release_date = None, sv = None,
-                              file_type_id = None, 
-                              data_source_id = None, 
-                              source_id = None):
-    command = "{hiduu_path}hi-data-upload-utility".format(
-      hiduu_path=HIDUU_PATH) + upload_sync + "-said" + source_id + "-f" + file_path + "-sas" + system_secret + "-re" + reason + "-sid"+ upload_sync_id
-    os.system(command)
-
-
-
-def upload_dataset_file_win(upload_sync = 'uploadDataSetFile', 
-                            upload_sync_id = None, 
-                            system_secret = None,
-                            file_path = None, 
-                            release_date = None, 
-                            sv = None, 
-                            file_type_id = None,
-                            data_source_id = None, 
-                            source_id = None):
-    command = "{hiduu_path}hi-data-upload-utility ".format(
-        hiduu_path=HIDUU_PATH) + upload_sync + " -said " + source_id + " -sas " + system_secret + " -sid " + upload_sync_id + " -f " + file_path + " -rl " + release_date + " -sv " + sv + " -fid " + file_type_id + " -dsid " + data_source_id
-    os.system(command)
-
