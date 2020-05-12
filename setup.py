4c9fe93011e115d2a0ca6c8a3a620fbcc98e7d4e
patrickgagnon <plgagnon00@gmail.com>
2020-05-12 17:09:21 -0400

    Setup 1.0.5

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-#requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
-
 setup(
     name="HIDUU",
-    version="1.0.4",
+    version="1.0.5",
     author="Patrick Gagnon",
     author_email="plgagnon00@gmail.com",
     description="A package to upload information to Cerner HealtheIntent",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/patrickgagnon/HIDUU",
-    packages=find_packages(),
-#    install_requires=requirements.txt,
+    packages=['hiduu'],
     classifiers=[ ],
 )
