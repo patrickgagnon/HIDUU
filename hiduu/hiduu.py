fbb8ce09a6a0137b8bd78a25be85f90fdfd4095b
patrickgagnon <plgagnon00@gmail.com>
2020-05-12 12:50:45 -0400

    Added string for win cmnd

--- hiduu/hiduu.py
+++ hiduu/hiduu.py
@@ -1,7 +1,6 @@
 from subprocess import check_call
 import os
 
-
 def hiduu_upload(hiduu_path = None,
                  upload_command = None,
                  system_account_id = None,
@@ -68,13 +67,10 @@ def hiduu_upload_win(hiduu_path = None,
                      args = None,
                      encryption = None,
                      client_mnemonic = None):
-    command = ["{hiduu_path}hi-data-upload-utility".format(hiduu_path=hiduu_path),
-               "{upload_command}".format(upload_command=upload_command),
-               " -said ", system_account_id,
-               " -sas ", system_account_secret,
-               " -sid ", source_id]
+    command = "{hiduu_path}hi-data-upload-utility".format(hiduu_path=hiduu_path) + "{upload_command}".format(
+        upload_command=upload_command) + " -said " + system_account_id + " -sas " + system_account_secret + " -sid " + source_id
     if upload_command == 'uploadUnvettedFile':
-        command +" -f " + file + " -re " + reason
+        command + " -f " + file + " -re " + reason
 
     if upload_command == 'uploadDataSetFile':
         command + " -dsid " + data_set_id + " -sv " + spec_version + " -fid " + file_id + " -rl " + file_release + " -f " + file
